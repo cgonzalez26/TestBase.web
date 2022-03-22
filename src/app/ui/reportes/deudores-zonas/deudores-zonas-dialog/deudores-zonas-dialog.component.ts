0cd655312af0fbfd153c34c1dc0c59f9c5435f5a
cgonzalez26 <crisgonzalez26@gmail.com>
2022-03-22 18:35:04 -0300

    dialogo deudor

--- src/app/ui/reportes/deudores-zonas/deudores-zonas-dialog/deudores-zonas-dialog.component.ts
+++ src/app/ui/reportes/deudores-zonas/deudores-zonas-dialog/deudores-zonas-dialog.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Deudores } from 'app/models/titulares/deudores';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+
+export interface DialogData {
+  titleTranslationCode: string;
+  action: string;
+  form: Deudores;
+}
+
 
 @Component({
-  selector: 'app-deudores-zonas-dialog',
+  selector: 'deudores-zonas-dialog',
   templateUrl: './deudores-zonas-dialog.component.html',
   styleUrls: ['./deudores-zonas-dialog.component.scss']
 })
 export class DeudoresZonasDialogComponent implements OnInit {
+  @BlockUI('form-dialog-deudores') dialogBlockUI: NgBlockUI;
+  messages = {
+    emptyMessage: 'No se encontraron registros'
+  };
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private _dialogData: DialogData,
+ 
+  ) { 
+    this.dialogBlockUI.start('Cargando...');
 
-  constructor() { }
+    this.dialogBlockUI.stop();
+  }
 
   ngOnInit(): void {
   }
